2fe25245905217069e42c5178379bea4a5684b0b
Siddhant12345678 <97388304+Siddhant12345678@users.noreply.github.com>
2023-06-13 15:32:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function gotoac(){
+    window.location="ac.html";
+}
+function gotobedroom(){
+    window.location="bedroom.html";
+}
+function gotobottles(){
+    window.location="bottles.html";
+}
+function gototv(){
+    window.location="tv.html";
+}
+function gotodesk(){
+    window.location="desk.html";
+}
